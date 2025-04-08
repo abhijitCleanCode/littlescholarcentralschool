5f3a05f664c5d141cb55a8d5882e2e108424367a
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2025-04-08 19:46:38 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar'
+import Navbar from "./components/Navbar"
 
 import SchoolIntroduction from './components/Intro'
 import MissionSection from './components/ourMission'
